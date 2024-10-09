c3a523198a10d264fa3080770d705ae88849972e
yihang.wang@nlpr.ia.ac.cn <yihang.wang@nlpr.ia.ac.cn>
2024-10-09 11:59:36 +0800

    Update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="OpenDTB-Framework",
     version="0.2.5",
     description="The underlying framework of CASIA's digital twin brain platform",
-    author="Brain Net Group",
+    author="Brain Netom Center",
     url="https://github.com/OpenDTB/OpenDTB-Framework",
     install_requires=["traits>=6.4.1", "ulid-py"],
     packages=setuptools.find_packages(),
